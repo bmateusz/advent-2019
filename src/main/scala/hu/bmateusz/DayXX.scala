26c9d8e65f7881f36a5942c9f1c9a3b4985e4a59
Mate Borcsok <mate.borcsok@prezi.com>
2019-12-04 00:43:05 +0100

    Small template for days

---
+++ src/main/scala/hu/bmateusz/DayXX.scala
@@ -0,0 +1,10 @@
+package hu.bmateusz
+
+object DayXX {
+
+  /** https://adventofcode.com/2019/day/ */
+  def main(args: Array[String]): Unit = {
+    val input = FileOperations.readResource("day01.txt")
+  }
+
+}
